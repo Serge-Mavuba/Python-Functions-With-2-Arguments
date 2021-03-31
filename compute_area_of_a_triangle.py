8d70738e6097ab7069fec422310742c1688196e3
Serge-Mavuba <79016443+Serge-Mavuba@users.noreply.github.com>
2021-03-31 00:52:42 -0400

    Update compute_area_of_a_triangle.py

--- compute_area_of_a_triangle.py
+++ compute_area_of_a_triangle.py
@@ -1,6 +1,11 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+#TO COMPUTE THE AREA WE NEED TO ARGUMENTS THE BASE (b) AND THE HEIGHT (h)
 def triangle_area(b,h):
     """Returns the value of a triangle with base b and height h."""
     return 0.5 * b * h
     
 triangle_area(3,6)
-print(triangle_area(3,6))
+print(Fore.GREEN + Style.BRIGHT + str(triangle_area(3,6)))
